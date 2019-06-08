ffd699249f6d19cc8414e86cc563253de3b4b76d
Bruno Morales <morales96bruno@gmail.com>
2019-06-08 19:38:22 -0300

    Update volumecontrol.sh

--- volumecontrol.sh
+++ volumecontrol.sh
@@ -5,10 +5,12 @@ MUTE="$2"
 
 if [ -z $MUTE ]
 then
+  for SINK in `pacmd list-sinks | grep 'index:' | cut -b12-`
   do
     pactl set-sink-volume $SINK $VOLUME
   done
 else
+  for SINK in `pacmd list-sinks | grep 'index:' | cut -b12-`
   do
     pactl set-sink-mute $SINK toggle
   done
